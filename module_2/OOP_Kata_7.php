e122adf23562800b31a88ba5b849008794582f7a
Vitalii Kemeniash <Vitalii_Kemeniash@epam.com>
2021-02-16 09:58:26 +0200

    module_2

---
+++ module_2/OOP_Kata_7.php
@@ -0,0 +1,39 @@
+<?php
+class Person{
+	const species = "Homo Sapiens";
+
+	public $name;
+	public $age;
+	public $occupation;
+
+	public function __construct($name, $age, $occupation){
+		$this->name = $name;
+		$this->age = $age;
+		$this->occupation = $occupation;
+	}
+
+	public function introduce(){
+		return 'Hello, my name is ' . $this->name;
+	}
+
+	public final function describe_job(){
+		return 'I am currently working as a(n) ' . $this->occupation;
+	}
+
+	public static final function greet_extraterrestrials($species){
+
+		return "Welcome to Planet Earth " . $species . "!";
+	}
+
+}
+
+class ComputerProgrammer extends Person{
+
+	public function __construct($name, $age, $occupation = "Computer Programmer"){
+		parent::__construct($name, $age, $occupation);
+	}
+
+	public function introduce(){
+		return parent::introduce() . " and I am a ". $this->occupation;
+	}
+}
